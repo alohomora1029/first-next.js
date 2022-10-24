845c40d48d882bfab4ca125220a554302e779e73
natsuki <06un21@gmail.com>
2022-10-25 00:32:32 +0900

    first commit

---
+++ pages/about/index.js
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+import Content from "../components/constent";
+
+export default function About() {
+  return (
+    <Content>
+      <Header title="About" />
+    </Content>
+  );
+}
