bbcb6a20f19b8b031ef8e5fa9343b82077f261ab
natsuki <06un21@gmail.com>
2022-10-26 10:04:40 +0900

    second commit

--- pages/index.js
+++ pages/index.js
@@ -6,29 +6,35 @@ import useSWR from "swr";
 
 export default function Home() {
   let title = "ともすた";
-  const{data,error} =useSWR('/api/message')
-  if(error)return<div>failed to load</div>
-  if(!data)return<div>loading...</div>
+  // const { data, error } = useSWR("/api/message");
+  // if (error) return <div>failed to load</div>;
+  // if (!data) return <div>loading...</div>;
 
   return (
     <Content>
+
       <Head>
         <title>{title}</title>
       </Head>
+      <div class="flex flex-col items-center ">
+
+      <p class="mt-8 text-3xl text-[#f87171] underline">Hello world!</p>
+      
+ <p class="mt-8">let</p> 
+<button class="my-8
+bg-indigo-500 sm:bg-green-400 md:bg-pink-300 lg:bg-blue-300 
+p-7 hover:bg-gray-300 cursor-pointer
+ text-xl text-white/[0.9] w-30 rounded-xl shadow-xl 
+">button</button>
+</div>
       <Header title={title} />
-      <p>{data.message}</p>
+      {/* <p>{data.message}</p> */}
       <title>{title}</title>
       <p>学ぶちゃんと</p>
       <Link href="about/">
         <a>About</a>
       </Link>
-      <style jsx>
-        {`
-          h1 {
-            color: red;
-          }
-        `}
-      </style>
+     
     </Content>
   );
 }
