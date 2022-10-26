cb11462bc1e93d252b0cecf8d3b10b63c03eca15
natsuki <06un21@gmail.com>
2022-10-26 12:52:02 +0900

    modified

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,33 @@
 import Head from "next/head";
+import Link from "next/Link";
 import Header from "./components/Header";
 import Content from "./components/constent";
 import useSWR from "swr";
 
 export default function Home() {
   let title = "ともすた";
-  const fetcher = (url) => fetch(url).then((res) => res.json());
-  const { data, error } = useSWR("/api/message", fetcher);
+  const fetcher =(url)=>fetch(url).then(res => res.json())
+  const { data, error } = useSWR("/api/message",fetcher);
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
 
   return (
     <Content>
+
       <Head>
         <title>{title}</title>
       </Head>
       <div className="flex flex-col items-center ">
-        <p className="mt-8 text-3xl text-[#f87171] underline">Hello world!</p>
 
-        <p className="mt-8">let</p>
-        <button
-          className="my-8
+      <p className="mt-8 text-3xl text-[#f87171] underline">Hello world!</p>
+      
+ <p className="mt-8">let</p> 
+<button className="my-8
 bg-indigo-500 sm:bg-green-400 md:bg-pink-300 lg:bg-blue-300 
 p-7 hover:bg-gray-300 cursor-pointer
  text-xl text-white/[0.9] w-30 rounded-xl shadow-xl 
-"
-        >
-          button
-        </button>
-      </div>
+">button</button>
+</div>
       <Header title={title} />
       <p>{data.message}</p>
       <title>{title}</title>
@@ -36,6 +35,7 @@ p-7 hover:bg-gray-300 cursor-pointer
       <Link href="about/">
         <a>About</a>
       </Link>
+     
     </Content>
   );
 }
